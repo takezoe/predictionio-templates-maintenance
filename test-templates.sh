2133f11ccac5d09548d6e06562f4f81eed59eaac
Naoki Takezoe <takezoe@gmail.com>
2018-09-23 22:11:11 +0900

    Update test script to run publishLocal for PredictionIO

--- test-templates.sh
+++ test-templates.sh
@@ -2,6 +2,11 @@
 SCRIPT_DIR=$(cd $(dirname $0); pwd)
 source $SCRIPT_DIR/env.sh
 
+cd $SCRIPT_DIR
+git clone -b develop https://git-wip-us.apache.org/repos/asf/predictionio.git
+cd predictionio
+sbt publishLocal
+
 for PIO_TEMPLATE in $PIO_TEMPLATES; do
   echo "[INFO] Test $PIO_TEMPLATE..."
   cd $SCRIPT_DIR/$PIO_TEMPLATE
