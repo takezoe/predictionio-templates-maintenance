62a2554b9764d6e0db4b18f91ddd72242c8e1cc0
Naoki Takezoe <takezoe@gmail.com>
2018-09-23 22:11:53 +0900

    Update PredictionIO and Spark version

--- env.sh
+++ env.sh
@@ -1,6 +1,6 @@
 #!/bin/#!/bin/sh
-export PIO_VERSION="0.14.0"
-export SPARK_VERSION="2.1.2"
+export PIO_VERSION="0.13.0"
+export SPARK_VERSION="2.1.1"
 export PIO_TEMPLATES="predictionio-template-attribute-based-classifier \
 predictionio-template-ecom-recommender \
 predictionio-template-java-ecom-recommender \
