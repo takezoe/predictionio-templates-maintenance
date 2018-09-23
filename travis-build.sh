74515ce0743033889ee67457157fee8e7d9c7336
Naoki Takezoe <takezoe@gmail.com>
2018-09-23 23:07:24 +0900

    Update for Travis

--- travis-build.sh
+++ travis-build.sh
@@ -7,6 +7,7 @@ cd $SCRIPT_DIR
 ./clone-templates.sh
 ./update-versions.sh
 
+rm -rf predictionio
 git clone -b develop https://git-wip-us.apache.org/repos/asf/predictionio.git
 cd predictionio
 sbt publishLocal
