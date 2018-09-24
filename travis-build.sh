e7c9686eeec0d4128cc99ae459a21c5f7031b3b5
Naoki Takezoe <takezoe@gmail.com>
2018-09-24 11:25:26 +0900

    Enable pio publishing

--- travis-build.sh
+++ travis-build.sh
@@ -2,10 +2,10 @@
 SCRIPT_DIR=$(cd $(dirname $0); pwd)
 cd $SCRIPT_DIR
 
-#rm -rf predictionio
-#git clone -b develop https://git-wip-us.apache.org/repos/asf/predictionio.git
-#cd predictionio
-#sbt publishLocal
+rm -rf predictionio
+git clone -b develop https://git-wip-us.apache.org/repos/asf/predictionio.git
+cd predictionio
+sbt publishLocal
 
 cd $SCRIPT_DIR
 ./clone-templates.sh
